b27ff8f8fc4ecd6113d456bef6d721ebb57a2707
Amdubaloch <amdubaloch2005@gmail.com>
2025-04-26 23:55:57 +0500

    Update message_formatter.py

--- src/message_formatter/message_formatter.py
+++ src/message_formatter/message_formatter.py
@@ -1,11 +1,11 @@
 def funny_deco(func):
     def wrapper(message):
-        border = " 🦧 🗣️" * (len(message) //2)
-        return f"{border}\n 🤓 {message} 🥱\n {border}"
+        border = "💓💨 " * (len(message) // 2)
+        return f"{border}\n 💓 {message} 💓\n {border}"
     return wrapper
 
 
 def PyarBhara_deco(func):
     def wrapper(message):
-        return f"💓💨💃 {message} 💓💨💃"
+        return f"🦧 🗣️ {message} 🦧 🗣️"
     return wrapper
