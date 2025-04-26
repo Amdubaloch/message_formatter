f3db5b30bdee164c017180f5714ed99ff708d752
Amdubaloch <amdubaloch2005@gmail.com>
2025-04-26 23:54:04 +0500

    update

--- src/message_formatter/message_formatter.py
+++ src/message_formatter/message_formatter.py
@@ -1,6 +1,7 @@
 def funny_deco(func):
     def wrapper(message):
-        return f"🦧🗣️ {message}🤓🥱"
+        border = " 🦧 🗣️" * (len(message) //2)
+        return f"{border}\n 🤓 {message} 🥱\n {border}"
     return wrapper
 
 
